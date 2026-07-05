7471605c5144822288bb16032088a2ffebd5953f
RichieFireball <83175346+RichieFireball@users.noreply.github.com>
2026-07-05 17:33:13 -0400

    add secure Paddle checkout page (#5)

---
+++ checkout/config.js
@@ -0,0 +1,5 @@
+window.GAMEFEED_PADDLE_CONFIG = Object.freeze({
+  clientToken: "live_bc481099ef323b68403ce60000c",
+  premiumPriceId: "pri_01kwt2tgzfvc6ybr2xswr4nb2w",
+  purchasesEnabled: false,
+});
